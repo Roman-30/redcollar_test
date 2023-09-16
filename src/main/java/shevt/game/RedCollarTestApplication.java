3c6ea133b214b01f8da3c9f0f1c0fffe26b98a23
Roman-30 <Romses2016@yandex.ru>
2023-09-16 15:21:04 +0300

    add spring console

--- src/main/java/shevt/game/RedCollarTestApplication.java
+++ src/main/java/shevt/game/RedCollarTestApplication.java
@@ -1,18 +1,27 @@
 package shevt.game;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.Banner;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import shevt.game.model.Graph;
 import shevt.game.service.GameService;
 
+@RequiredArgsConstructor
 @SpringBootApplication
-public class RedCollarTestApplication {
+public class RedCollarTestApplication implements CommandLineRunner {
+    private final GameService gs;
 
     public static void main(String[] args) {
-        SpringApplication.run(RedCollarTestApplication.class, args);
-        GameService gs = new GameService();
+        SpringApplication app = new SpringApplication(RedCollarTestApplication.class);
+        app.setBannerMode(Banner.Mode.OFF);
+        app.run(args);
+    }
+
+    @Override
+    public void run(String... args) {
         Graph game = gs.createGame();
         gs.runGame(game);
     }
-
 }
