78c718d8c8919c6620e55c2dc95a819604039d62
Roman-30 <Romses2016@yandex.ru>
2023-09-16 15:19:14 +0300

    add method crete data base

--- src/main/java/shevt/game/service/GraphService.java
+++ src/main/java/shevt/game/service/GraphService.java
@@ -1,5 +1,23 @@
 package shevt.game.service;
 
-public class GraphService {
+import org.springframework.stereotype.Component;
+import shevt.game.model.AnimalNode;
+import shevt.game.model.FactNode;
+import shevt.game.model.Graph;
 
+@Component
+public class GraphService {
+    public Graph createDataBase() {
+        Graph graph = new Graph();
+
+        AnimalNode cat = new AnimalNode("Кот");
+        AnimalNode whale = new AnimalNode("Кит");
+        FactNode factNode = new FactNode("живет на суше");
+
+        factNode.getAnswerToAnimalMap().put(true, cat);
+        factNode.getAnswerToAnimalMap().put(false, whale);
+
+        graph.getFactNodes().add(factNode);
+        return graph;
+    }
 }
