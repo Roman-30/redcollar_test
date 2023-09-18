487e65082778c3f716a121ff17671fafbb0a72ba
Roman-30 <Romses2016@yandex.ru>
2023-09-18 17:14:42 +0300

    fix method save and load game

--- src/main/java/shevt/game/service/SaveService.java
+++ src/main/java/shevt/game/service/SaveService.java
@@ -1,22 +1,32 @@
 package shevt.game.service;
 
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Component;
 import shevt.game.model.Graph;
 
 import java.io.*;
 
+@Component
 public class SaveService {
 
-    private static final String FILE_PATH = "src/main/resources/statemant/state.ser";
+    public static final String FILE_NAME = "config/state.ser";
 
-    public void saveGame(Graph dataBase, String path) throws IOException {
-        FileOutputStream outputStream = new FileOutputStream(path);
+    public void saveGame(Graph dataBase, String fileName) throws IOException {
+        Resource resource = new ClassPathResource(fileName);
+        File file = resource.getFile();
+
+        FileOutputStream outputStream = new FileOutputStream(file);
         ObjectOutputStream objectOutputStream = new ObjectOutputStream(outputStream);
         objectOutputStream.writeObject(dataBase);
         objectOutputStream.close();
     }
 
-    public Graph loadGame(String path) throws IOException, ClassNotFoundException {
-        FileInputStream fileInputStream = new FileInputStream(path);
+    public Graph loadGame(String fileName) throws IOException, ClassNotFoundException {
+        Resource resource = new ClassPathResource(fileName);
+        File file = resource.getFile();
+
+        FileInputStream fileInputStream = new FileInputStream(file);
         ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
         return (Graph) objectInputStream.readObject();
     }
